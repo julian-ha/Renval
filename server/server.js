1fca92d2182648e29fb5a9b2adc551e5d7a6dfab
Julian Häring <Julian.Haering@plan-b-gmbh.com>
2020-05-04 16:51:50 +0200

    asdf

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname , "..", "build")));
+
+const HTML_FILE = path.join(__dirname, "..", "build", "index.html");
+console.log(HTML_FILE);
 
 // create a GET route
-app.get('/express_backend', (req, res) => {
+app.get('/api', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
+app.get('*', (req, res, next) => {
+  res.sendFile(HTML_FILE);
 });
 
 app.listen(port, () => {
