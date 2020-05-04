f2cbd56fbe1fd15df2620468b7a5a2b84f93d0ed
Julian Häring <Julian.Haering@plan-b-gmbh.com>
2020-05-04 16:37:53 +0200

    asd

---
+++ build/precache-manifest.6ef92736ae7a0484c782897b8c4cf7fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4904e93258e1707c6ed05e9cc43ae0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44708401251cbf70eb42",
+    "url": "/static/css/2.c7d7c3df.chunk.css"
+  },
+  {
+    "revision": "5f747191c7a8d7f33d44",
+    "url": "/static/css/main.631a6b8b.chunk.css"
+  },
+  {
+    "revision": "44708401251cbf70eb42",
+    "url": "/static/js/2.3c6605b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3c6605b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f747191c7a8d7f33d44",
+    "url": "/static/js/main.e95cc563.chunk.js"
+  },
+  {
+    "revision": "5d485be25fa7de93280d",
+    "url": "/static/js/runtime-main.c0770fd8.js"
+  }
+]);
